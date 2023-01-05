84c1d5c50c62034fbf6656115013c35b686b84ff
Mohammad <mhmmoshtaghi@gmail.com>
2023-01-05 14:57:19 +0330

    Add anytree to parser

--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,13 +1,13 @@
 from scanner.config import *
 from scanner.scanner import Scanner
 import json
+from anytree import RenderTree, Node
 
 
 class Parser:
 
     def __init__(self):
         self.scanner = Scanner("input.txt")
-        parse_tree_str = ""
         with open('./parser/grammar/table.json') as f:
             grammar_contents = f.read()
             f.close()
@@ -19,6 +19,7 @@ class Parser:
         self.grammar = grammar['grammar']
         self.parse_table = grammar['parse_table']
         self.stack = ['0']
+        self.nodes = {}
 
     def get_next_token(self):
         current_token = self.scanner.get_next_token()
@@ -26,17 +27,14 @@ class Parser:
         while current_token and (current_token[0] == WHITESPACE or current_token[0] == COMMENT):
             current_token = self.scanner.get_next_token()
 
-        if not current_token:
-            return '$'
-        if current_token[0] == KEYWORD or current_token[0] == SYMBOL:
-            return current_token[1]
-        return current_token[0]
+        return current_token
 
     def parse(self):
         current_token = self.get_next_token()
+        self.create_node(current_token)
         while True:
             last_index = self.stack[-1]
-            next_move = self.parse_table[last_index][current_token].split('_')
+            next_move = self.parse_table[last_index][Parser.get_token_value(current_token)].split('_')
             if next_move[0] == "accept":
                 print("Accept")
                 return
@@ -44,6 +42,7 @@ class Parser:
                 self.stack.append(current_token)
                 self.stack.append(next_move[1])
                 current_token = self.get_next_token()
+                self.create_node(current_token)
             elif next_move[0] == 'reduce':
                 rule = self.grammar[next_move[1]]
                 left_rule, right_rule = rule[0], rule[2:]
@@ -55,3 +54,16 @@ class Parser:
                 if next_move[0] != 'goto':
                     raise Exception("goto error")
                 self.stack.append(next_move[1])
+
+    def create_node(self, token):
+        token_value = Parser.get_token_value(token)
+        node = Node(token_value)
+        self.nodes[token] = token_value
+
+    @staticmethod
+    def get_token_value(token):
+        if not token:
+            return '$'
+        if token[0] == KEYWORD or token[0] == SYMBOL:
+            return token[1]
+        return token[0]
