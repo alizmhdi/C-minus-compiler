44fc0ba08c67dca861a3b423ec71b0adc98eb745
Mohammad <mhmmoshtaghi@gmail.com>
2023-01-07 18:54:51 +0330

    Add error handler

--- src/compiler.py
+++ src/compiler.py
@@ -4,8 +4,7 @@ from parser.parser import Parser
 # Mohammad  Moshtaghi   99109047
 # Mahdi     Alizadeh    99101932
 
-if __name__ == '__main__':
-    DFA()
-    parser = Parser()
-    parser.parse()
-
+# if __name__ == '__main__':
+DFA()
+parser = Parser()
+parser.parse()
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -2,6 +2,8 @@ from scanner.config import *
 from scanner.scanner import Scanner
 import json
 from anytree import Node, RenderTree
+from writer.syntax_error_writer import SyntaxErrorWriter
+from writer.parse_tree_writer import ParseTreeWriter
 
 
 class Parser:
@@ -19,6 +21,10 @@ class Parser:
         self.grammar = grammar['grammar']
         self.parse_table = grammar['parse_table']
         self.stack = ['0']
+        self.gotos = self.goto_states()
+        self.errors = []
+        self.syntax_error_writer = SyntaxErrorWriter('syntax_errors.txt')
+        self.parse_tree_writer = ParseTreeWriter('parse_tree.txt')
 
     def get_next_token(self):
         current_token = self.scanner.get_next_token()
@@ -32,10 +38,36 @@ class Parser:
         current_token = self.get_next_token()
         while True:
             last_index = self.stack[-1]
-            next_move = self.parse_table[last_index][Parser.get_token_value(current_token)].split('_')
+            try:
+                next_move = self.parse_table[last_index][Parser.get_token_value(current_token)].split('_')
+            except Exception:
+                self.errors.append(f"#{current_token[2]} : syntax error , illegal {Parser.get_token_value(current_token)}")
+                current_token = self.get_next_token()
+                while self.stack[-1] not in self.gotos:
+                    self.stack.pop()
+                    removed = self.stack.pop()
+                    if removed.name in self.non_terminal:
+                        self.errors.append(f"syntax error , discarded {removed.name} from stack")
+                    else:
+                        self.errors.append(f"syntax error , discarded ({removed.name[0]}, {removed.name[1]}) from stack")
+                while not self.is_in_follows(self.stack[-1], Parser.get_token_value(current_token)):
+                    if Parser.get_token_value(current_token) == '$':
+                        self.errors.append(f"#{current_token[2]} : syntax error , Unexpected EOF")
+                        self.syntax_error_writer.write(self.errors)
+                        return
+                    self.errors.append(f"#{current_token[2]} : syntax error , discarded {current_token[1]} from input")
+                    current_token = self.get_next_token()
+                next_non_terminal = self.is_in_follows(self.stack[-1], Parser.get_token_value(current_token))
+                next_state = self.parse_table[self.stack[-1]][next_non_terminal].split('_')[1]
+                self.errors.append(f"#{current_token[2]} : syntax error , missing {next_non_terminal}")
+                self.stack.append(Node(next_non_terminal))
+                self.stack.append(next_state)
+
+                continue
             if next_move[0] == "accept":
                 Node('$', left_rule_node)
-                print(Parser.format_tree(left_rule_node))
+                self.parse_tree_writer.write(Parser.format_tree(left_rule_node))
+                self.syntax_error_writer.write(self.errors)
                 return
             elif next_move[0] == 'shift':
                 if current_token:
@@ -55,11 +87,42 @@ class Parser:
                         item = self.stack.pop(start_reduce_index)
                         if i % 2 == 0:
                             item.parent = left_rule_node
+                else:
+                    Node('epsilon', left_rule_node)
                 next_move = self.parse_table[self.stack[-1]][left_rule].split('_')
                 self.stack.append(left_rule_node)
                 if next_move[0] != 'goto':
                     raise Exception("goto error")
                 self.stack.append(next_move[1])
+
+    def goto_states(self) -> list:
+        result = []
+
+        for state in self.parse_table.keys():
+            row = self.parse_table[state]
+            for word in row.values():
+                if word.startswith('goto'):
+                    result.append(state)
+                    break
+
+        return result
+
+    def get_gotos_alphabetically(self, state: str):
+        result = []
+        state_row = self.parse_table[state]
+        for word in state_row.keys():
+            if state_row[word].startswith('goto'):
+                result.append(word)
+        result.sort()
+        return result
+
+    def is_in_follows(self, state: str, token):
+        gotos = self.get_gotos_alphabetically(state)
+        for goto in gotos:
+            if token in self.follow[goto]:
+                return goto
+
+        return None
 
     @staticmethod
     def format_tree(root: Node):
@@ -76,7 +139,7 @@ class Parser:
 
     @staticmethod
     def get_token_value(token):
-        if not token:
+        if token[0] == EOF:
             return '$'
         if token[0] == KEYWORD or token[0] == SYMBOL:
             return token[1]
