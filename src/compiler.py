038e23e331f69dfa65822c00be684c14d4615625
alizademhdi <alizademhdi@gmail.com>
2023-01-07 14:29:18 +0330

    generate parse tree

--- src/compiler.py
+++ src/compiler.py
@@ -2,7 +2,7 @@ from scanner.dfa import DFA
 from parser.parser import Parser
 
 # Mohammad  Moshtaghi   99109047
-# Mahdi     Alizadeh    ....
+# Mahdi     Alizadeh    99101932
 
 if __name__ == '__main__':
     DFA()
--- src/parser/parser.py
+++ src/parser/parser.py
@@ -1,7 +1,7 @@
 from scanner.config import *
 from scanner.scanner import Scanner
 import json
-from anytree import RenderTree, Node
+from anytree import Node, RenderTree
 
 
 class Parser:
@@ -19,7 +19,6 @@ class Parser:
         self.grammar = grammar['grammar']
         self.parse_table = grammar['parse_table']
         self.stack = ['0']
-        self.nodes = {}
 
     def get_next_token(self):
         current_token = self.scanner.get_next_token()
@@ -31,34 +30,49 @@ class Parser:
 
     def parse(self):
         current_token = self.get_next_token()
-        self.create_node(current_token)
         while True:
             last_index = self.stack[-1]
             next_move = self.parse_table[last_index][Parser.get_token_value(current_token)].split('_')
             if next_move[0] == "accept":
-                print("Accept")
+                Node('$', left_rule_node)
+                print(Parser.format_tree(left_rule_node))
                 return
             elif next_move[0] == 'shift':
-                self.stack.append(current_token)
+                if current_token:
+                    node = Node((current_token[0], current_token[1]))
+                else:
+                    node = Node(current_token)
+                self.stack.append(node)
                 self.stack.append(next_move[1])
                 current_token = self.get_next_token()
-                self.create_node(current_token)
             elif next_move[0] == 'reduce':
                 rule = self.grammar[next_move[1]]
                 left_rule, right_rule = rule[0], rule[2:]
+                left_rule_node = Node(left_rule)
                 if right_rule != ['epsilon']:
-                    for _ in range(2 * len(right_rule)):
-                        self.stack.pop()
+                    start_reduce_index = len(self.stack) - 2 * len(right_rule)
+                    for i in range(2 * len(right_rule)):
+                        item = self.stack.pop(start_reduce_index)
+                        if i % 2 == 0:
+                            item.parent = left_rule_node
                 next_move = self.parse_table[self.stack[-1]][left_rule].split('_')
-                self.stack.append(left_rule)
+                self.stack.append(left_rule_node)
                 if next_move[0] != 'goto':
                     raise Exception("goto error")
                 self.stack.append(next_move[1])
 
-    def create_node(self, token):
-        token_value = Parser.get_token_value(token)
-        node = Node(token_value)
-        self.nodes[token] = token_value
+    @staticmethod
+    def format_tree(root: Node):
+        result = ''
+        for pre, fill, node in RenderTree(root):
+            result += '%s%s' % (pre, Parser.format_node_name(node)) + '\n'
+        return result
+
+    @staticmethod
+    def format_node_name(node):
+        if type(node.name) == tuple:
+            return f'({node.name[0]}, {node.name[1]})'
+        return node.name
 
     @staticmethod
     def get_token_value(token):
